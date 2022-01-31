a708247a1c11059d36d4204873411930219ae950
atinn <tinateen584@gmail.com>
2022-01-31 19:59:14 +0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,48 @@
-import react from 'react'
+import React, { Component } from 'react';
 
-function Header(){
-    return(
-    <>
-    <h1>My Day is not your Day</h1>
-    </>
+
+export default class Header extends Component {
+    Navbar(){
+      return(
+        <div className="nav">
+            <div className='kiri'>
+              <i className="fas fa-bars"></i>
+              <a href="">
+              <button onClick="show">IMDb</button>
+              </a> 
+            </div> 
+            <div className='tengah'>
+                <div className='inputan'>
+                    <div className='category'>
+                      All
+                    </div>
+                    <div className='search'>
+                      <input  name="" placeholder='Search IMDb'></input>
+                    </div>
+                    <div className='s'>
+                      <i class="fas fa-search"></i>
+                    </div>
+                </div>
+            </div>
+            <div className='kanan'>
+                <div className='pro link'>
+                  <a href="">IMDbPro</a>
+                </div>
+                <div className='watch link'>
+                <a href="">WatchList</a>
+                </div>
+                <div className='signIn link'>
+                <a href="">Sign In</a>
+                </div>
+            </div>
+        </div>
+      )
+    }
+  render() {
+    return( 
+    <div>
+      <this.Navbar/>
+    </div>
     )
+  }
 }
-
-export default Header
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './styles/style.css'
+// import App from './App';
+import '@fortawesome/fontawesome-free/js/all.js';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header/>
   </React.StrictMode>,
   document.getElementById('root')
 );
