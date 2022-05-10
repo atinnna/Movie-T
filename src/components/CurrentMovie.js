d89712b84212f7ff03f5b0672bbfcf571d2af393
atinn <tinateen584@gmail.com>
2022-05-10 23:51:12 +0700

    update

--- src/components/CurrentMovie.js
+++ src/components/CurrentMovie.js
@@ -8,8 +8,8 @@ function View(){
     let key = "fb280e17a4edec2501eec3c356448bf9"
     const param = useParams()
     const [movie,setMovie] = useState([])
-    const [arrmov,setArr] = useState([])
-    
+    const [arrmov,setGenre] = useState([])
+    const [arrpro,setProduction] = useState([])
     useEffect(()=>{
         axios.get(`https://api.themoviedb.org/3/movie/${param.id}?api_key=${key}&append_to_response=videos,images`)
         .then(res=>{
@@ -22,27 +22,32 @@ function View(){
     useEffect(()=>{
         axios.get(`https://api.themoviedb.org/3/movie/${param.id}?api_key=${key}&append_to_response=videos,images`)
         .then(res=>{
-            const kk = res.data.genres
-            setArr(kk)
+            const genr = res.data.genres
+            setGenre(genr)
+        })
+        .catch(err=>console.log(err))
+    },[])    
+    
+    useEffect(()=>{
+        axios.get(`https://api.themoviedb.org/3/movie/${param.id}?api_key=${key}&append_to_response=videos,images`)
+        .then(res=>{
+            const pc = res.data.production_companies
+            setProduction(pc)
         })
         .catch(err=>console.log(err))
     },[])  
 
-    const hh = arrmov.forEach(d=>{
-        return(
-            <spa>{d.name}</spa>
-        )
-    })
-    console.log(hh)
+  
     return(
-        <div className='main'>
+        <>
+        <div className='main' style={{margin:0,padding:0,backgroundImage:`url(https://image.tmdb.org/t/p/original/${movie.poster_path})`,backgroundPosition:'center',backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
+                 <div className='title-cur'>
+                    <h2>{movie.original_title}</h2>
+                    <span>{movie.release_date}</span>==
+                </div>
         <div className='grid'>
             <Img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} className="cMo-img" alt=""/>
             <div className='m-deskripsi'>
-                <p>
-                    <h2>{movie.original_title}</h2><br/>
-                    <span>{movie.release_date}</span>
-                </p>
                 <div className='flex-des'>
                     <ul>
                         <li>Genre</li>
@@ -52,25 +57,33 @@ function View(){
                         <li>Vote</li>
                     </ul>
                     <ul>
-                        <li>{ 
-                      hh
-                    
-                        }
-                        </li>
-                        <li>Nama Produksi</li>
-                        <li>Pendapatan</li>
-                        <li>Status</li>
-                        <li>Vote</li>
+                        <li>:</li>
+                        <li>:</li>
+                        <li>:</li>
+                        <li>:</li>
+                        <li>:</li>
                     </ul>
+                    <ul>
+                        <li>{ arrmov.map(d=><span>{d.name}, </span>)}
+                        </li>
+                        <li>{ arrpro.map(p=><span>{p.name}, </span>)}</li>
+                        <li>{movie.revenue}</li>
+                        <li>{movie.status}</li>
+                        <li>{movie.vote_average} dari {movie.vote_count} orang</li>
+                    </ul>
+                </div>
+                <div className='overview'>
+                    <h2>Overview</h2>
+                    <p>{movie.overview}</p>
                 </div>
             </div>
         </div>
-        <div className='overview'>
-            <p>{movie.overview}</p>
-        </div>
         </div> 
+       
+       
+        </>
     )
-                    }
+        }
 
 
 export default class CurrentMovie extends Component {
@@ -78,9 +91,7 @@ export default class CurrentMovie extends Component {
         
         return (
             <div>
-                alal
                 <View/>
-                
             </div>
         )
     }
